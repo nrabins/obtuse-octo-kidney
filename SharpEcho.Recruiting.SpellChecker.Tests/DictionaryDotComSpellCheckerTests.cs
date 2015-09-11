e4b43c98663798c2153ae06d09a79973fe3f0eb7
Nathan Rabins <nrabins@gmail.com>
2015-09-10 21:39:44 -0500

    Initial commit.

---
+++ SharpEcho.Recruiting.SpellChecker.Tests/DictionaryDotComSpellCheckerTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+using SharpEcho.Recruiting.SpellChecker.Contracts;
+using SharpEcho.Recruiting.SpellChecker.Core;
+
+namespace SharpEcho.Recruiting.SpellChecker.Tests
+{
+    [TestFixture]
+    class DictionaryDotComSpellCheckerTests
+    {
+        private ISpellChecker SpellChecker;
+
+        [TestFixtureSetUp]
+        public void TestFixureSetUp()
+        {
+            SpellChecker = new DictionaryDotComSpellChecker();
+        }
+
+        [Test]
+        public void Check_That_SharpEcho_Is_Misspelled()
+        {
+            Assert.False(SpellChecker.Check("SharpEcho"));
+        }
+
+        [Test]
+        public void Check_That_South_Is_Not_Misspelled()
+        {
+            Assert.IsTrue(SpellChecker.Check("South"));
+        }
+    }
+}
---
+++ SharpEcho.Recruiting.SpellChecker.Tests/MnemonicSpellCheckerIBeforeETests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+using SharpEcho.Recruiting.SpellChecker.Contracts;
+using SharpEcho.Recruiting.SpellChecker.Core;
+
+namespace SharpEcho.Recruiting.SpellChecker.Tests
+{
+    [TestFixture]
+    class MnemonicSpellCheckerIBeforeETests
+    {
+        private ISpellChecker SpellChecker;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            SpellChecker = new MnemonicSpellCheckerIBeforeE();
+        }
+
+        [Test]
+        public void Check_Word_That_Contains_I_Before_E_Is_Spelled_Correctly()
+        {
+            Assert.True(SpellChecker.Check("believe"));
+            Assert.True(SpellChecker.Check("fierce"));
+            Assert.True(SpellChecker.Check("collie"));
+            Assert.True(SpellChecker.Check("die"));
+            Assert.True(SpellChecker.Check("friend"));
+            Assert.True(SpellChecker.Check("deceive"));
+            Assert.True(SpellChecker.Check("ceiling"));
+            Assert.True(SpellChecker.Check("receipt"));
+        }
+
+        [Test]
+        public void Check_Word_That_Contains_I_Before_E_Is_Spelled_Incorrectly()
+        {
+            Assert.False(SpellChecker.Check("heir"));
+            Assert.False(SpellChecker.Check("protein"));
+            Assert.False(SpellChecker.Check("science"));
+            Assert.False(SpellChecker.Check("seeing"));
+            Assert.False(SpellChecker.Check("their"));
+            Assert.False(SpellChecker.Check("veil"));
+        }
+    }
+}
